4db6e413b03a0ec803ffd09c76a5c1cfc5921b00
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2022-03-24 23:43:41 +0200

    Do the Homepage controller and views

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,23 +6,7 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
+    public function index() {
         return view('home');
     }
 }
