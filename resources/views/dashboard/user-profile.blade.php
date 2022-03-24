bfabecfb01ec0ad2ca603b95ebb250b8ea94071e
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2022-03-24 23:42:33 +0200

    Do the dashboard and user-profile views

---
+++ resources/views/dashboard/user-profile.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">{{ __('User Profile') }}</div>
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
