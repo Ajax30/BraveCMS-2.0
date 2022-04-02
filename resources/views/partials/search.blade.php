563099d9321d79f94bb1703ab049a7383487f5a3
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2022-04-02 19:11:05 +0300

    Bugfix: add forgotten action atribute to the search form

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -1,4 +1,4 @@
-<form method="GET" action="" id="search_form" class="w-100 py-1 pe-md-2" accept-charset="utf-8">
+<form method="GET" action="{{ route('dashboard.articles') }}" id="search_form" class="w-100 py-1 pe-md-2" accept-charset="utf-8">
     <div id="group-search" class="input-group">
         <input class="form-control form-control-dark" type="text" name="search" placeholder="Search articles..." aria-label="Search">
         <div class="input-group-append">
