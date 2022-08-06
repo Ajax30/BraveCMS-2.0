8abfdda13989617c09f197db4760fa234a25ea1d
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2022-08-06 10:12:09 +0300

    Fix the PagesController controller

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,15 +9,10 @@ class PagesController extends FrontendController
   public function page($id) {
 		$page = Page::firstWhere('id', $id);
 		return view('themes/' . $this->theme_directory . '/templates/page', 
-			[
-				'theme_directory' => $this->theme_directory,
-				'site_name' => $this->site_name,
-				'tagline' => $this->tagline,
-				'owner_name' => $this->owner_name,
-				'pages' => $this->pages,
-				'categories' => $this->article_categories,
-				'page' => $page
-			]
-		);
+			array_merge($this->data, [
+				'page' => $page,
+				'tagline' => $page->title,
+				])
+			);
 	}
 }
