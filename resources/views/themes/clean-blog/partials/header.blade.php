fcd6a5e72f193095731c0b02b11f2a98bf2022a7
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2022-08-12 15:27:04 +0300

    Minify the Clean-blog theme CSS

--- resources/views/themes/clean-blog/partials/header.blade.php
+++ resources/views/themes/clean-blog/partials/header.blade.php
@@ -15,7 +15,7 @@
 			<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 		
 			<!-- Custom styles for this template -->
-			<link href="{{ asset('themes/' . $theme_directory . '/css/clean-blog.css') }}" rel="stylesheet">
+			<link href="{{ asset('themes/' . $theme_directory . '/css/clean-blog.min.css') }}" rel="stylesheet">
 </head>
 <body>
 
