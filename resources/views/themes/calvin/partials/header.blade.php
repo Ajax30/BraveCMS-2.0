fff44ab1a67b858fd88fefd59ab0d6a0c353bc9b
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2022-08-21 14:13:57 +0300

    Fix the missing icons issue

--- resources/views/themes/calvin/partials/header.blade.php
+++ resources/views/themes/calvin/partials/header.blade.php
@@ -25,10 +25,10 @@
 
     <!-- favicons
     ================================================== -->
-    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
-    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32"  href="{{ asset('themes/' . $theme_directory . '/icons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16"  href="{{ asset('themes/' . $theme_directory . '/icons/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('themes/' . $theme_directory . '/site.webmanifest') }}">
 
 </head>
 
