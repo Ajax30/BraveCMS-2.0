9c43f6178de866efe2acfc8a92f9e1235f5c70ac
Tim Brownlaw <timbrownlaw@gmail.com>
2022-09-08 12:39:11 +1000

    FIX: Broken HTML The Hero Blade appeared above the HTML Head.

--- resources/views/themes/calvin/layout.blade.php
+++ resources/views/themes/calvin/layout.blade.php
@@ -1,9 +1,13 @@
 @include('themes/' . $theme_directory . '/partials/header')
 
+@if (!isset($category) && !isset($author))
+    @include('themes/' . $theme_directory . '/partials/hero')
+@endif
+
 <!-- Main Content -->
 @yield('content')
 
-@include('themes/' . $theme_directory . '/partials/footer') 
+@include('themes/' . $theme_directory . '/partials/footer')
 
 
 
