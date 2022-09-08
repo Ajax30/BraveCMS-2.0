8c32e937720f09cd773ad03decad6b717f0a62b9
Tim Brownlaw <timbrownlaw@gmail.com>
2022-09-08 10:50:10 +1000

    FIX: inifinite-comments.js only loaded for single.blade.php. Added section for custom_js_files to yield in footer.blade.php

--- resources/views/themes/calvin/templates/single.blade.php
+++ resources/views/themes/calvin/templates/single.blade.php
@@ -1,83 +1,86 @@
+@extends('themes/' .$theme_directory . '/layout')
 
-  @extends('themes/' .$theme_directory . '/layout')
+@section('content')
 
-	@section('content')
-
-		<section class="s-content">
-      <div class="row">
-        <div class="column large-12">
-          <article class="s-content__entry format-standard">
-            <div class="s-content__media">
-              <div class="s-content__post-thumb">
-                <img src="{{ asset('images/articles/' . $article->image) }}" alt="{{ $article->title }}">
-              </div>
-            </div>
-            <!-- end s-content__media -->
-            <div class="s-content__entry-header">
-              <h1 class="s-content__title s-content__title--post">
-								{{ $article->title }}
-							</h1>
-            </div>
-            <!-- end s-content__entry-header -->
-            <div class="s-content__primary">
-              <div class="s-content__entry-content">
-                {!! $article->content !!}
-              </div>
-              <!-- end s-entry__entry-content -->
-              <div class="s-content__entry-meta">
-                <div class="entry-author meta-blk">
-                  <div class="author-avatar">
-                    <img class="avatar" src="{{ asset('images/avatars/' . $article->user->avatar) }}" alt="{{ $article->user->first_name }} {{ $article->user->last_name }}">
-                  </div>
-                  <div class="byline">
-                    <span class="bytext">Posted By</span>
-                    <a href="#0">
-											{{ $article->user->first_name }} {{ $article->user->last_name }}
-										</a>
-                  </div>
-                </div>
-
-                <div class="meta-bottom">
-                  <div class="entry-cat-links meta-blk">
-                    <div class="cat-links">
-                      <span>In</span>
-                      <a href="{{ url('/category/' . $article->category->id) }}">{{ $article->category->name }}</a>
+    <section class="s-content">
+        <div class="row">
+            <div class="column large-12">
+                <article class="s-content__entry format-standard">
+                    <div class="s-content__media">
+                        <div class="s-content__post-thumb">
+                            <img src="{{ asset('images/articles/' . $article->image) }}" alt="{{ $article->title }}">
+                        </div>
                     </div>
-                    <span>On</span>
-                    {{ date('j F, Y', strtotime($article->created_at)) }}
-                  </div>
-                </div>
-              </div><!-- s-content__entry-meta -->
-              <div class="s-content__pagenav">
-                @if($old_article)
-                <div class="next-nav">
-                  <a href="{{ url('/show/' . $old_article->slug) }}">
-                  <span>Older</span>
-                    {{ $old_article->title }}
-                  </a>
-                </div>
-                @endif
+                    <!-- end s-content__media -->
+                    <div class="s-content__entry-header">
+                        <h1 class="s-content__title s-content__title--post">
+                            {{ $article->title }}
+                        </h1>
+                    </div>
+                    <!-- end s-content__entry-header -->
+                    <div class="s-content__primary">
+                        <div class="s-content__entry-content">
+                            {!! $article->content !!}
+                        </div>
+                        <!-- end s-entry__entry-content -->
+                        <div class="s-content__entry-meta">
+                            <div class="entry-author meta-blk">
+                                <div class="author-avatar">
+                                    <img class="avatar" src="{{ asset('images/avatars/' . $article->user->avatar) }}" alt="{{ $article->user->first_name }} {{ $article->user->last_name }}">
+                                </div>
+                                <div class="byline">
+                                    <span class="bytext">Posted By</span>
+                                    <a href="#0">
+                                        {{ $article->user->first_name }} {{ $article->user->last_name }}
+                                    </a>
+                                </div>
+                            </div>
 
-                @if($new_article)
-                <div class="next-nav">
-                  <a href="{{ url('/show/' . $new_article->slug) }}">
-                  <span>Newer</span>
-                  {{ $new_article->title }}
-                  </a>
-                </div>
-                @endif
-              </div>
-              <!-- end s-content__pagenav -->
-            </div>
-            <!-- end s-content__primary -->
-          </article><!-- end entry -->
-        </div><!-- end column -->
-      </div><!-- end row -->
+                            <div class="meta-bottom">
+                                <div class="entry-cat-links meta-blk">
+                                    <div class="cat-links">
+                                        <span>In</span>
+                                        <a href="{{ url('/category/' . $article->category->id) }}">{{ $article->category->name }}</a>
+                                    </div>
+                                    <span>On</span>
+                                    {{ date('j F, Y', strtotime($article->created_at)) }}
+                                </div>
+                            </div>
+                        </div><!-- s-content__entry-meta -->
+                        <div class="s-content__pagenav">
+                            @if($old_article)
+                                <div class="next-nav">
+                                    <a href="{{ url('/show/' . $old_article->slug) }}">
+                                        <span>Older</span>
+                                        {{ $old_article->title }}
+                                    </a>
+                                </div>
+                            @endif
 
-			@include('themes/' . $theme_directory . '/partials/comments')
+                            @if($new_article)
+                                <div class="next-nav">
+                                    <a href="{{ url('/show/' . $new_article->slug) }}">
+                                        <span>Newer</span>
+                                        {{ $new_article->title }}
+                                    </a>
+                                </div>
+                            @endif
+                        </div>
+                        <!-- end s-content__pagenav -->
+                    </div>
+                    <!-- end s-content__primary -->
+                </article><!-- end entry -->
+            </div><!-- end column -->
+        </div><!-- end row -->
+
+        @include('themes/' . $theme_directory . '/partials/comments')
     </section>
-<script>
-  let article_id = '{{$article->id}}';
-  let token = "{{ csrf_token() }}";
-</script>
-    @endsection
+    <script>
+        let article_id = '{{$article->id}}';
+        let token = "{{ csrf_token() }}";
+    </script>
+@endsection
+
+@section('custom_js_files')
+    <script src="{{ asset('themes/' . $theme_directory . '/js/infinite-comments.js') }}"></script>
+@endsection
