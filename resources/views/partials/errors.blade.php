3c8c141a83872e8d90db108b832b173af5831588
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2023-01-07 23:30:39 +0200

    Do not allow inactive users to login

--- resources/views/partials/errors.blade.php
+++ resources/views/partials/errors.blade.php
@@ -1,6 +1,14 @@
 <div class="container">
+  @if(request()->routeIs('login'))
+  <div class="row justify-content-center">  
+    <div class="col-md-6">
+  @endif
     <div class="alert alert-danger alert-dismissible">
-        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-        <p class="my-0 text-center">{{ session('error') }}</p>
+      <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+      <p class="my-0 text-center">{{ session('error') }}</p>
     </div>
+  @if(request()->routeIs('login'))
+    </div>
+  </div>
+  @endif
 </div>
