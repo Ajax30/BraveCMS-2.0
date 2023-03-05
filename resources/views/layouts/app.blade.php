b24cce508cb33c2b3a69d4fe0bdc85abcb541415
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2023-03-05 11:01:25 +0200

    Fix layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,8 @@
 		<div class="container">
 			<div class="row justify-content-center">
 				{{-- 6 columns for user profile --}}
-				@if(request()->routeIs(['dashboard.settings','user','register','login','password*']))<div class="col-md-6">@else<div class="col-md-12">@endif
-						
+				@if(request()->routeIs(['dashboard.settings','user','register','login','password*']))
+          <div class="col-md-6">@else<div class="col-md-12">@endif
 						@if(!request()->routeIs(['dashboard.settings','user','register','login','password*']))
 						<div class="row">
 							<div class="col-sm-4 col-md-3 d-none d-sm-block">
@@ -22,13 +22,14 @@
 							</div>
 						@endif
 						
-						@if(!request()->routeIs(['dashboard.settings','user','register','login','password*']))<div class="col-sm-8 col-md-9">@endif
+						@if(!request()->routeIs(['dashboard.settings','user','register','login','password*']))
+              <div class="col-sm-8 col-md-9">@endif
 								@yield('content')
-						@if(!request()->routeIs(['dashboard.settings','user','register','login','password*']))</div>@endif
+						@if(!request()->routeIs(['dashboard.settings','user','register','login','password*']))
+              </div>@endif
 						</div>
 					</div>
-				</div>
-		</div>
+		  </div>
 </main>
 
 @include('partials/footer')
