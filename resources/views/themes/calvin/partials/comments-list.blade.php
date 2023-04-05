b596e5602e3ea1273d35dc17bb9e289b7246a154
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2023-04-05 12:27:39 +0300

    No comment reply link

--- resources/views/themes/calvin/partials/comments-list.blade.php
+++ resources/views/themes/calvin/partials/comments-list.blade.php
@@ -8,9 +8,6 @@
               <div class="comment__author">{{ $comment->user->first_name }} {{ $comment->user->last_name }}</div>
               <div class="comment__meta">
                   <div class="comment__time">{{ date('jS M Y', strtotime($comment->created_at)) }}</div>
-                  <div class="comment__reply">
-                      <a class="comment-reply-link" href="#0">Reply</a>
-                  </div>
               </div>
           </div>
           <div class="comment__text">
