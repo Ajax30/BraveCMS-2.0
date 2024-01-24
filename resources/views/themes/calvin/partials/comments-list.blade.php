0aa925b156823121dcf40b1658315787fd999eca
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2024-01-24 17:24:40 +0200

    Reformat code

--- resources/views/themes/calvin/partials/comments-list.blade.php
+++ resources/views/themes/calvin/partials/comments-list.blade.php
@@ -7,38 +7,43 @@
             </div>
             <div class="comment__content">
                 <div class="comment__info">
-                  <div class="comment__author">{{ $comment->user->first_name }} {{ $comment->user->last_name }}</div>
-                  <div class="comment__meta">
-                      <div class="comment__time">{{ date('jS M Y', strtotime($comment->created_at)) }}</div>
-                      @auth
-                        <div class="comment__reply">
-                          @if ($comment->user->id !== Auth::user()->id)
-                              <a class="comment-reply-link" href="#0">
-                                <i class="fa fa-comment"></i> Reply
-                            </a> 
-                          @endif
-                            
-                          @if ($comment->user->id == Auth::user()->id)
-                              <a href="#0" class="comment-edit-link">
-                                <i class="fa fa-edit"></i> Edit
-                              </a>
-                              @include('themes/' . $theme_directory . '/partials/comment-delete-form', ['commentOrReply' => $comment])
-                          @endif
-                        </div>
-                      @endauth
-                  </div>
+                    <div class="comment__author">{{ $comment->user->first_name }} {{ $comment->user->last_name }}</div>
+                    <div class="comment__meta">
+                        <div class="comment__time">{{ date('jS M Y', strtotime($comment->created_at)) }}</div>
+                        @auth
+                            <div class="comment__reply">
+                                @if ($comment->user->id !== Auth::user()->id)
+                                    <a class="comment-reply-link" href="#0">
+                                        <i class="fa fa-comment"></i> Reply
+                                    </a>
+                                @endif
+
+                                @if ($comment->user->id == Auth::user()->id)
+                                    <a href="#0" class="comment-edit-link">
+                                        <i class="fa fa-edit"></i> Edit
+                                    </a>
+                                    @include(
+                                        'themes/' . $theme_directory . '/partials/comment-delete-form',
+                                        ['commentOrReply' => $comment]
+                                    )
+                                @endif
+                            </div>
+                        @endauth
+                    </div>
                 </div>
                 <div class="comment__text">
-                  <p>{{ $comment->body }}</p>
+                    <p>{{ $comment->body }}</p>
                 </div>
             </div>
 
             @auth
-              @include('themes/' . $theme_directory . '/partials/comment-form')
+                @include('themes/' . $theme_directory . '/partials/comment-form')
 
-              @if ($comment->user->id == Auth::user()->id)
-                @include('themes/' . $theme_directory . '/partials/comment-edit-form', ['commentOrReply' => $comment])
-              @endif
+                @if ($comment->user->id == Auth::user()->id)
+                    @include('themes/' . $theme_directory . '/partials/comment-edit-form', [
+                        'commentOrReply' => $comment,
+                    ])
+                @endif
             @endauth
 
             {{-- Comment replies --}}
@@ -60,12 +65,17 @@
                                         </div>
                                         @auth
                                             <div class="comment__reply">
-                                              @if ($reply->user->id == Auth::user()->id)
-                                                <a href="#0" class="comment-edit-link">
-                                                  <i class="fa fa-edit"></i> Edit
-                                                </a>
-                                                @include('themes/' . $theme_directory . '/partials/comment-delete-form', ['commentOrReply' => $reply])
-                                              @endif
+                                                @if ($reply->user->id == Auth::user()->id)
+                                                    <a href="#0" class="comment-edit-link">
+                                                        <i class="fa fa-edit"></i> Edit
+                                                    </a>
+                                                    @include(
+                                                        'themes/' .
+                                                            $theme_directory .
+                                                            '/partials/comment-delete-form',
+                                                        ['commentOrReply' => $reply]
+                                                    )
+                                                @endif
                                             </div>
                                         @endauth
                                     </div>
@@ -76,9 +86,11 @@
                             </div>
 
                             @auth
-                              @if ($reply->user->id == Auth::user()->id)
-                                @include('themes/' . $theme_directory . '/partials/comment-edit-form', ['commentOrReply' => $reply])
-                              @endif
+                                @if ($reply->user->id == Auth::user()->id)
+                                    @include('themes/' . $theme_directory . '/partials/comment-edit-form', [
+                                        'commentOrReply' => $reply,
+                                    ])
+                                @endif
                             @endauth
                         </li>
                     @endforeach
