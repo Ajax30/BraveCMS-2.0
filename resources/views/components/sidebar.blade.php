a4221290c3e3f7ac4d83a468008fa43d47bee577
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2024-06-07 22:07:20 +0300

    Format sidebar code

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,39 +1,43 @@
 <div class="sidebar card shadow-sm">
-	<div class="card-header px-2">{{ __('Sidebar') }}</div>
-	<div class="card-body p-0">
-		<ul class="list-group list-group-flush">
-			<li class="list-group-item">
-				<a class="{{ request()->routeIs('dashboard.pages*') ? 'active' : '' }}" href="{{ route('dashboard.pages') }}">
-					<span>
-            <i class="far fa-file-alt pe-1"></i> Pages
-          </span>
-					<span class="badge">{{ $page_count }}</span>
-				</a>
-			</li> 
-			<li class="list-group-item">
-				<a class="{{ request()->routeIs('dashboard.articles*') ? 'active' : '' }}" href="{{ route('dashboard.articles') }}">
-					<span>
-            <i class="far fa-newspaper pe-1"></i> Articles
-          </span>
-					<span class="badge">{{ $article_count }}</span>
-				</a>
-			</li>
-			<li class="list-group-item">
-				<a class="{{ request()->routeIs('dashboard.categories*') ? 'active' : '' }}" href="{{ route('dashboard.categories') }}">
-					<span>
-            <i class="far fa-list-alt pe-1"></i> Categories
-          </span>
-					<span class="badge">{{ $category_count }}</span>
-				</a>
-			</li>
-			<li class="list-group-item">
-				<a class="{{ request()->routeIs('dashboard.comments*') ? 'active' : '' }}" href="{{ route('dashboard.comments') }}">
-					<span>
-            <i class="far fa-comments pe-1"></i> Comments
-          </span>
-					<span class="badge">{{ $comments_count }}</span>
-				</a>
-			</li>
-		</ul>
-	</div>
+    <div class="card-header px-2">{{ __('Sidebar') }}</div>
+    <div class="card-body p-0">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+                <a class="{{ request()->routeIs('dashboard.pages*') ? 'active' : '' }}"
+                    href="{{ route('dashboard.pages') }}">
+                    <span>
+                        <i class="far fa-file-alt pe-1"></i> Pages
+                    </span>
+                    <span class="badge">{{ $page_count }}</span>
+                </a>
+            </li>
+            <li class="list-group-item">
+                <a class="{{ request()->routeIs('dashboard.articles*') ? 'active' : '' }}"
+                    href="{{ route('dashboard.articles') }}">
+                    <span>
+                        <i class="far fa-newspaper pe-1"></i> Articles
+                    </span>
+                    <span class="badge">{{ $article_count }}</span>
+                </a>
+            </li>
+            <li class="list-group-item">
+                <a class="{{ request()->routeIs('dashboard.categories*') ? 'active' : '' }}"
+                    href="{{ route('dashboard.categories') }}">
+                    <span>
+                        <i class="far fa-list-alt pe-1"></i> Categories
+                    </span>
+                    <span class="badge">{{ $category_count }}</span>
+                </a>
+            </li>
+            <li class="list-group-item">
+                <a class="{{ request()->routeIs('dashboard.comments*') ? 'active' : '' }}"
+                    href="{{ route('dashboard.comments') }}">
+                    <span>
+                        <i class="far fa-comments pe-1"></i> Comments
+                    </span>
+                    <span class="badge">{{ $comments_count }}</span>
+                </a>
+            </li>
+        </ul>
+    </div>
 </div>
