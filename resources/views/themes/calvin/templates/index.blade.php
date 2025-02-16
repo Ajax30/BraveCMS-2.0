f36bf793efef1165c2ebf630139909a8be2c2db5
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2025-02-16 19:37:50 +0200

    Format index templates for both themes

--- resources/views/themes/calvin/templates/index.blade.php
+++ resources/views/themes/calvin/templates/index.blade.php
@@ -1,55 +1,54 @@
-@extends('themes/' .$theme_directory . '/layout')
+@extends('themes/' . $theme_directory . '/layout')
 
 @section('hero')
-
-@if (!isset($category) && !isset($author) && !isset($tag))
-		@include('themes/' . $theme_directory . '/partials/hero')
-	@endif
+    @if (!isset($category) && !isset($author) && !isset($tag))
+        @include('themes/' . $theme_directory . '/partials/hero')
+    @endif
 @endsection
 
 @section('content')
 
-<!-- content
-    ================================================== -->
+    <!-- content
+        ================================================== -->
     <section class="s-content s-content--no-top-padding">
 
         <!-- masonry
-        ================================================== -->
+            ================================================== -->
         <div class="s-bricks">
 
-          @if(isset($category) or isset($tag) or isset($author) or isset($search_query))
+            @if (isset($category) or isset($tag) or isset($author) or isset($search_query))
+                <div class="s-pageheader">
+                    <div class="row">
+                        <div class="column large-12">
+                            <h1 class="page-title">
+                                @isset($category)
+                                    <span class="page-title__small-type">Category</span>
+                                    {{ $category->name }}
+                                @endisset
 
-          <div class="s-pageheader">
-            <div class="row">
-                <div class="column large-12">
-                    <h1 class="page-title">
-                        @isset($category)
-                          <span class="page-title__small-type">Category</span>
-                          {{ $category->name }}
-                        @endisset
+                                @isset($tag)
+                                    <span class="page-title__small-type">Tag</span>
+                                    {{ $tag->name }}
+                                @endisset
 
-                        @isset($tag)
-                          <span class="page-title__small-type">Tag</span>
-                          {{ $tag->name }}
-                        @endisset
+                                @isset($author)
+                                    <span class="page-title__small-type">Author</span>
+                                    {{ $author->first_name }} {{ $author->last_name }}
+                                @endisset
 
-                        @isset($author)
-                          <span class="page-title__small-type">Author</span>
-                          {{ $author->first_name }} {{ $author->last_name }}
-                        @endisset
-
-                        @if (isset($search_query))
-                          Search
-                          <span class="page-title__small-type">
-                            We found {{ $article_count ? $article_count : 'no'  }} {{ $article_count > 1 ? 'articles' : 'article' }} containing <span class="quote-inline">{{ $search_query }}</span>
-                          </span>
-                        @endif
-                    </h1>
+                                @if (isset($search_query))
+                                    Search
+                                    <span class="page-title__small-type">
+                                        We found {{ $article_count ? $article_count : 'no' }}
+                                        {{ $article_count > 1 ? 'articles' : 'article' }} containing <span
+                                            class="quote-inline">{{ $search_query }}</span>
+                                    </span>
+                                @endif
+                            </h1>
+                        </div>
+                    </div>
                 </div>
-            </div>
-          </div>
-      
-          @endif
+            @endif
 
 
             <div class="masonry">
@@ -64,57 +63,58 @@
                     </div>
 
                     @if (count($articles))
-											@foreach ($articles as $article)
+                        @foreach ($articles as $article)
+                            <article class="brick entry" data-aos="fade-up">
 
-											<article class="brick entry" data-aos="fade-up">
+                                <div class="entry__thumb">
+                                    <a href="{{ url('/show/' . $article->slug) }}" class="thumb-link">
+                                        <img src="{{ asset('images/articles/' . $article->image) }}"
+                                            alt="{{ $article->title }}">
+                                    </a>
+                                </div> <!-- end entry__thumb -->
 
-													<div class="entry__thumb">
-															<a href="{{ url('/show/' . $article->slug) }}" class="thumb-link">
-																	<img src="{{ asset('images/articles/' . $article->image) }}"  alt="{{ $article->title }}">
-															</a>
-													</div> <!-- end entry__thumb -->
+                                <div class="entry__text">
+                                    <div class="entry__header">
+                                        <h1 class="entry__title">
+                                            <a href="{{ url('/show/' . $article->slug) }}">
+                                                {{ $article->title }}
+                                            </a>
+                                        </h1>
 
-													<div class="entry__text">
-															<div class="entry__header">
-																	<h1 class="entry__title">
-																		<a href="{{ url('/show/' . $article->slug) }}">
-																			{{ $article->title }}
-																		</a>
-																	</h1>
+                                        <div class="entry__meta">
+                                            <span class="byline">By:
+                                                <span class='author'>
+                                                    <a href="{{ url('/author/' . $article->user->id) }}">{{ $article->user->first_name }}
+                                                        {{ $article->user->last_name }}</a>
+                                                </span>
+                                            </span>
+                                            <span class="cat-links">
+                                                <a
+                                                    href="{{ url('/category/' . $article->category->id) }}">{{ $article->category->name }}</a>
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div class="entry__excerpt">
+                                        <p>{{ $article->short_description }}</p>
+                                    </div>
+                                    <a class="entry__more-link" href="{{ url('/show/' . $article->slug) }}">Learn More</a>
+                                </div> <!-- end entry__text -->
 
-																	<div class="entry__meta">
-																			<span class="byline">By:
-																					<span class='author'>
-																							<a href="{{ url('/author/' . $article->user->id) }}">{{ $article->user->first_name }} {{ $article->user->last_name }}</a>
-																			</span>
-																	</span>
-																			<span class="cat-links">
-																					<a href="{{ url('/category/' . $article->category->id) }}">{{ $article->category->name }}</a>
-																			</span>
-																	</div>
-															</div>
-															<div class="entry__excerpt">
-																	<p>{{ $article->short_description }}</p>
-															</div>
-															<a class="entry__more-link" href="{{ url('/show/' . $article->slug) }}">Learn More</a>
-													</div> <!-- end entry__text -->
-
-											</article> <!-- end article -->
-
-											@endforeach
+                            </article> <!-- end article -->
+                        @endforeach
                     @endif
                 </div> <!-- end brick-wrapper -->
             </div> <!-- end masonry -->
 
-            @if($articles->hasPages())
-							<div class="row">
-								<div class="column large-12">
-										<nav class="pgn">
-											{!! $articles->onEachSide(1)->withQueryString()->links() !!}
-										</nav><!-- end pgn -->
-								</div><!-- end column -->
-							</div><!-- end row -->
-						@endif
+            @if ($articles->hasPages())
+                <div class="row">
+                    <div class="column large-12">
+                        <nav class="pgn">
+                            {!! $articles->onEachSide(1)->withQueryString()->links() !!}
+                        </nav><!-- end pgn -->
+                    </div><!-- end column -->
+                </div><!-- end row -->
+            @endif
 
         </div> <!-- end s-bricks -->
 
--- resources/views/themes/clean-blog/templates/index.blade.php
+++ resources/views/themes/clean-blog/templates/index.blade.php
@@ -1,79 +1,83 @@
-@extends('themes/' .$theme_directory . '/layout')
+@extends('themes/' . $theme_directory . '/layout')
 
 @section('content')
 
-<!-- Page Header -->
-<header class="masthead" style="background-image: url({{ asset('themes/' . $theme_directory . '/img/home-bg.jpg') }}">
-	<div class="overlay"></div>
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-8 col-md-10 mx-auto">
-				<div class="site-heading">
-					<h1>{{ $site_name }}</h1>
-					<span class="subheading">
-						@if(isset($category))
-							{{ $category->name }}
-						@elseif(isset($author))
-							Articles by {{ $author->first_name }} {{ $author->last_name }}
-						@else
-							{{ $tagline }}
-						@endif
-					</span>
-				</div>
-			</div>
-		</div>
-	</div>
-</header>
+    <!-- Page Header -->
+    <header class="masthead" style="background-image: url({{ asset('themes/' . $theme_directory . '/img/home-bg.jpg') }}">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-10 mx-auto">
+                    <div class="site-heading">
+                        <h1>{{ $site_name }}</h1>
+                        <span class="subheading">
+                            @if (isset($category))
+                                {{ $category->name }}
+                            @elseif(isset($author))
+                                Articles by {{ $author->first_name }} {{ $author->last_name }}
+                            @else
+                                {{ $tagline }}
+                            @endif
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 
-	<div class="container">
-    <div class="row">
-      <div class="col-lg-8 col-md-10 mx-auto">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
 
-				@if (isset($search_query))
-					<p class="mt-0 text-muted">
-            We found {{ $article_count ? $article_count : 'no'  }} {{ $article_count > 1 ? 'articles' : 'article' }} containing <span class="quote-inline">{{ $search_query }}</span>:
-          </p>
-				@endif
+                @if (isset($search_query))
+                    <p class="mt-0 text-muted">
+                        We found {{ $article_count ? $article_count : 'no' }}
+                        {{ $article_count > 1 ? 'articles' : 'article' }} containing <span
+                            class="quote-inline">{{ $search_query }}</span>:
+                    </p>
+                @endif
 
-				@if (count($articles))
-					@foreach ($articles as $article)
-						<div class="post-preview">
-							<a href="{{ url('/show/' . $article->slug) }}">
-								<h2 class="post-title">
-									{{ $article->title }}
-								</h2>
-								<h3 class="post-subtitle">
-									{{ $article->short_description }}
-								</h3>
-							</a>
-							<p class="post-meta">Posted by
-								<a href="{{ url('/author/' . $article->user->id) }}">{{ $article->user->first_name }} {{ $article->user->last_name }}</a>
-								on {{ date('j F, Y', strtotime($article->created_at)) }}
-							</p>
-						</div>
+                @if (count($articles))
+                    @foreach ($articles as $article)
+                        <div class="post-preview">
+                            <a href="{{ url('/show/' . $article->slug) }}">
+                                <h2 class="post-title">
+                                    {{ $article->title }}
+                                </h2>
+                                <h3 class="post-subtitle">
+                                    {{ $article->short_description }}
+                                </h3>
+                            </a>
+                            <p class="post-meta">Posted by
+                                <a href="{{ url('/author/' . $article->user->id) }}">{{ $article->user->first_name }}
+                                    {{ $article->user->last_name }}</a>
+                                on {{ date('j F, Y', strtotime($article->created_at)) }}
+                            </p>
+                        </div>
 
-						@if(!$loop->last)<hr>@endif
-						
-					@endforeach
-				@endif
+                        @if (!$loop->last)
+                            <hr>
+                        @endif
+                    @endforeach
+                @endif
 
-				<!-- Pager -->
-				@if($articles->hasPages())
-					<div class="clearfix">
-						<ul class="pagination">
-							<li class="next">
-								<a class="btn btn-primary {{ $articles->onFirstPage() ? 'disabled' : '' }}" href="{{ $articles->withQueryString()->previousPageUrl() }}">&larr; Newer Posts</a>
-							</li>
-							<li class="prev">
-								<a class="btn btn-primary {{ $articles->onLastPage() ? 'disabled' : '' }}" href="{{ $articles->withQueryString()->nextPageUrl() }}">Older Posts &rarr;</a>
-							</li>
-						</ul>
-					</div>
-				@endif
-      </div>
+                <!-- Pager -->
+                @if ($articles->hasPages())
+                    <div class="clearfix">
+                        <ul class="pagination">
+                            <li class="next">
+                                <a class="btn btn-primary {{ $articles->onFirstPage() ? 'disabled' : '' }}"
+                                    href="{{ $articles->withQueryString()->previousPageUrl() }}">&larr; Newer Posts</a>
+                            </li>
+                            <li class="prev">
+                                <a class="btn btn-primary {{ $articles->onLastPage() ? 'disabled' : '' }}"
+                                    href="{{ $articles->withQueryString()->nextPageUrl() }}">Older Posts &rarr;</a>
+                            </li>
+                        </ul>
+                    </div>
+                @endif
+            </div>
+        </div>
     </div>
-  </div>
-  <hr>
+    <hr>
 @endsection
-
-	
