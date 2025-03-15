5de5892215211d49c7faa99061e6a9ce6c926d4b
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2025-03-15 11:11:42 +0200

    Add article tags script to 'Add article' and 'Edit article' sections.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,10 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
@@ -23,6 +20,7 @@
         ]))
         <script src="{{ asset('lib/ckeditor/js/ckeditor.js') }}"></script>
     @endif
+
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Styles -->
