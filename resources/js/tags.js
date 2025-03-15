1adb6db32e956fda2dc2a87bb869d80e57794d6f
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2025-03-15 10:52:02 +0200

    Fix the tags update script

--- resources/js/tags.js
+++ resources/js/tags.js
@@ -2,10 +2,12 @@ const tagsList = document.querySelector(".tags-list")
 const tagActions = document.getElementById("tagActions")
 const tagSelector = document.getElementById("tags")
 const tagToggler = document.getElementById("tagSelectorToggler")
-var preSelectedTags = Array.from(tagSelector.options)
-  .filter((option) => option.selected)
-  .map((option) => option.text)
-var selectedTags = new Set()
+if (tagSelector) {
+  var preSelectedTags = Array.from(tagSelector.options)
+    .filter((option) => option.selected)
+    .map((option) => option.text)
+  var selectedTags = new Set()
+}
 
 window.filterTags = (event) => {
   var query = event.target.value
@@ -47,33 +49,41 @@ window.renderTags = () => {
       .join("") ||
     `<li class="text-muted">Use [Ctrl] + [Click] to select one or more tags from the list</li>`
   for (const option of tagSelector.options) {
-    option.selected = selectedTags.has(option.textContent)
+    option.selected = selectedTags.has(option.textContent.trim())
   }
 }
 
-window.addPreselectedTags = () => {
-  preSelectedTags.forEach(selectedTags.add.bind(selectedTags))
-  renderTags()
+if (preSelectedTags) {
+  window.addPreselectedTags = () => {
+    preSelectedTags.forEach(selectedTags.add.bind(selectedTags))
+    renderTags()
+  }
 }
 
-tagsList.addEventListener("click", function (event) {
-  if (event.target.tagName !== "BUTTON") return
-  let tagToRemove = event.target.closest("LI").children[0].textContent
-  let optionToDeselect = Array.from(tagSelector.options).find((option) => {
-    return option.innerText == tagToRemove
+if (tagsList) {
+  tagsList.addEventListener("click", function (event) {
+    if (event.target.tagName !== "BUTTON") return
+    let tagToRemove = event.target.closest("LI").children[0].textContent.trim()
+    let optionToDeselect = Array.from(tagSelector.options).find((option) => {
+      return option.innerText == tagToRemove
+    })
+    optionToDeselect.removeAttribute('selected')
+    selectedTags.delete(tagToRemove)
+    renderTags()
   })
-  optionToDeselect.removeAttribute('selected')
-  selectedTags.delete(tagToRemove)
-  renderTags()
-})
+}
 
-tagSelector.addEventListener("change", function () {
-  selectedTags = new Set(
-    Array.from(tagSelector.options)
-      .filter((option) => option.selected)
-      .map((option) => option.textContent),
-  )
-  renderTags()
-})
+if (tagSelector) {
+  tagSelector.addEventListener("change", function () {
+    selectedTags = new Set(
+      Array.from(tagSelector.options)
+        .filter((option) => option.selected)
+        .map((option) => option.textContent.trim()),
+    )
+    renderTags()
+  })
+}
 
-addPreselectedTags();
+if (tagSelector) {
+  window.addPreselectedTags();
+} 
