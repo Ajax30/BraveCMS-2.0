6882ec1d38bc328c942bba886c01098c29de1494
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2025-06-10 17:40:02 +0300

    Change the dashboard color scheme: use more green

--- resources/views/dashboard/pages.blade.php
+++ resources/views/dashboard/pages.blade.php
@@ -4,7 +4,7 @@
 	<div class="card shadow-sm">
 		<div class="card-header d-flex justify-content-between px-2">
 			<span class="align-self-center">{{ __('Pages') }}</span>
-			<a href="{{ route('dashboard.pages.new') }}" title="Add a new page" class="btn btn-sm btn-primary">
+			<a href="{{ route('dashboard.pages.new') }}" title="Add a new page" class="btn btn-sm btn-success">
 				<i class="fa-solid fa-circle-plus"></i> Add page
 			</a>
 		</div>
@@ -26,11 +26,11 @@
 							<td>{{ $page->title }}</td>
 							<td class="text-end">
 								<div class="btn-group btn-group-sm">
-									<a href="{{ route('dashboard.pages.edit', [$page->id]) }}" class="btn btn-primary">
+									<a href="{{ route('dashboard.pages.edit', [$page->id]) }}" class="btn btn-success">
 										<i class="fa-solid fa-pen-to-square"></i> Edit
 									</a>
 
-									<a href="{{ route('dashboard.pages.delete', [$page->id]) }}" class="btn btn-primary" onclick="return confirm('Delete this page?')" title="Delete article">
+									<a href="{{ route('dashboard.pages.delete', [$page->id]) }}" class="btn btn-success" onclick="return confirm('Delete this page?')" title="Delete article">
 										<i class="fa-solid fa-trash"></i> Delete
 									</a>
 								</div>
