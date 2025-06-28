c47100c42f0340eaadfc5881d7b81dad776bbcd8
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2025-06-28 22:52:07 +0300

    Do live validation (with JavaScript) for the 'Register' form

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,92 +6,96 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
+  use RegistersUsers;
 
-    use RegistersUsers;
+  /**
+   * Where to redirect users after registration.
+   *
+   * @var string
+   */
+  protected $redirectTo = RouteServiceProvider::DASHBOARD;
 
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::DASHBOARD;
+  /**
+   * Create a new controller instance.
+   */
+  public function __construct()
+  {
+    $this->middleware('guest');
+  }
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest');
+  /**
+   * Get a validator for an incoming registration request.
+   */
+  protected function validator(array $data)
+  {
+    $rules = [
+      'first_name' => ['required', 'string', 'max:255'],
+      'last_name' => ['required', 'string', 'max:255'],
+      'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+      'password' => ['required', 'string', 'min:6', 'confirmed', 'regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).*$/'],
+      'password_confirmation' => ['required', 'same:password'],
+      'accept' => ['accepted'],
+    ];
+
+    $messages = [
+      'first_name.required' => 'The "First name" field is required.',
+      'last_name.required' => 'The "Last name" field is required.',
+      'email.required' => 'Please provide a valid email address.',
+      'email.email' => 'The email address you provided is not valid.',
+      'email.unique' => 'The email address is already in use.',
+      'password.required' => 'A password is required',
+      'password.min' => 'The password must have at least :min characters.',
+      'password.regex' => 'Include uppercase and lowercase letters, at least one number and one symbol (special character).',
+      'accept.required' => 'You must accept the Terms & conditions of service.'
+    ];
+
+    return Validator::make($data, $rules, $messages);
+  }
+
+  /**
+   * Create a new user instance after a valid registration.
+   */
+  protected function create(array $data)
+  {
+    // All registered users have the role "Basic User",
+    // except the first, which is a "Super-admin"
+    $role_id = User::count() == 0 ? 4 : 1;
+
+    return User::create([
+      'role_id' => $role_id,
+      'first_name' => $data['first_name'],
+      'last_name' => $data['last_name'],
+      'email' => $data['email'],
+      'password' => Hash::make($data['password']),
+      'active' => 1
+    ]);
+  }
+
+  /**
+   * Check that email is unique.
+   */
+  public function checkEmail(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
+      'email' => ['required', 'email', 'unique:users,email'],
+    ]);
+
+    if ($validator->fails()) {
+      return response()->json([
+        'valid' => false,
+        'message' => 'The email address is already in use.',
+      ]);
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        $rules = [
-            'first_name' => ['required', 'string', 'max:255'],
-            'last_name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:6', 'confirmed', 'regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).*$/'],
-            'password_confirmation' => ['required', 'same:password'],
-            'accept' => ['accepted'],
-        ];
-
-        $messages = [
-            'first_name.required' => 'The "First name" field is required.',
-            'last_name.required' => 'The "Last name" field is required.',
-            'email.required' => 'Please provide a valid email address.',
-            'email.email' => 'The email address you provided is not valid.',
-            'email.unique' => 'The email address you provided is already in use.',
-            'password.required' => 'A password is required',
-            'password.min' => 'The password must have at least :min characters.',
-            'password.regex' => 'Include uppercase and lowercase letters, at least one number and one symbol (special character).',
-            'accept.required' => 'You must accept the Terms & conditions of service.'
-        ];
-        
-        return Validator::make($data, $rules, $messages);
-    }
-
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\Models\User
-     */
-    protected function create(array $data)
-    {
-        // All registered users have the role "Basic User",
-        // except the first, which is a "Super-admin"
-        $role_id = User::count() == 0 ? 4 : 1;
-
-        return User::create([
-          'role_id' => $role_id,
-          'first_name' => $data['first_name'],
-          'last_name' => $data['last_name'],
-          'email' => $data['email'],
-          'password' => Hash::make($data['password']),
-          'active' => 1
-        ]);
-    }
+    return response()->json([
+      'valid' => true,
+      'message' => '',
+    ]);
+  }
 }
