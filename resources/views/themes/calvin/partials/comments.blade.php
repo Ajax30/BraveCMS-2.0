aa91a46ee6ce4195bff3a7f7f172e8072ac16c8e
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2026-03-06 11:43:08 +0200

    Add disable/enable comments per article feature (#114)
    Add disable/enable comments per article feature

--- resources/views/themes/calvin/partials/comments.blade.php
+++ resources/views/themes/calvin/partials/comments.blade.php
@@ -3,12 +3,15 @@
 <div class="comments-wrap">
     <div class="row comment-respond">
         @auth
-            <!-- START respond -->
-            <div id="respond" class="column">
-                <h3>Add Comment</h3>
-
-                @include('themes/' . $theme_directory . '/partials/comment-form')
-            </div><!-- END respond-->
+            @if (!$article->disable_comments)
+                <!-- START respond -->
+                <div id="respond" class="column">
+                    <h3>Add Comment</h3>
+                    @include('themes/' . $theme_directory . '/partials/comment-form')
+                </div><!-- END respond-->
+            @else
+                <p class="h-text-center h-full-width">Adding comments is disabled for this article</p>
+            @endif
         @endauth
 
         @guest
@@ -20,25 +23,25 @@
     <div id="comments" class="row">
         <div class="column large-12">
             @if ($comments)
-              <h3>
-                @if ($comments_count === 0)
-                    No comments for this article yet
-                @else
-                    {{ $comments_count }} comment{{ $comments_count > 1 ? 's': ''}}
-                @endif
-              </h3>
-              <!-- START commentlist -->
-              <div id="commentsList">
-                  <ol class="commentlist {{ boolval($is_infinitescroll) ? 'infinite-scroll' : '' }}">
-                      @include('themes/' . $theme_directory . '/partials/comments-list')
-                  </ol>
-                  
-                  @if ($comments_count > $comments_per_page)
-                    <div class="ajax-load">
-                      <span></span>
-                    </div>
-                  @endif
-              </div>
+                <h3>
+                    @if ($comments_count === 0)
+                        No comments for this article yet
+                    @else
+                        {{ $comments_count }} comment{{ $comments_count > 1 ? 's' : '' }}
+                    @endif
+                </h3>
+                <!-- START commentlist -->
+                <div id="commentsList">
+                    <ol class="commentlist {{ boolval($is_infinitescroll) ? 'infinite-scroll' : '' }}">
+                        @include('themes/' . $theme_directory . '/partials/comments-list')
+                    </ol>
+
+                    @if ($comments_count > $comments_per_page)
+                        <div class="ajax-load">
+                            <span></span>
+                        </div>
+                    @endif
+                </div>
             @endif
         </div><!-- end col-full -->
     </div><!-- end comments -->
