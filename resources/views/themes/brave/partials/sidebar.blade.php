30efc9dd9c69a0c6bd1de13c0db6f443dc41be63
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2026-03-08 13:42:06 +0200

    Show the most recent articles in the sidebar of the Brave theme (#118)
    * Show the most recent articles in the sidebar of the Brave theme
    
    * Show featured articles in the sidebar of the Brave theme (instead of categories)
    
    * Show featured articles in the first sidebar tab (Brave theme)
    
    * Smooth transition between tabs (Brave theme)

--- resources/views/themes/brave/partials/sidebar.blade.php
+++ resources/views/themes/brave/partials/sidebar.blade.php
@@ -1,26 +1,51 @@
-<aside class="sidebar">     
+<aside class="sidebar">
     @include('themes/' . $theme_directory . '/partials/newsletter-form')
 
     <div class="tabbed-sidebar">
         <div class="tabbed-heading">
             <ul class="nav nav-tabs" role="tablist" data-bs-tabs="tabs">
+                @if (count($featured_articles))
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#featured" role="tab" data-bs-toggle="tab">Featured</a>
+                    </li>
+                @endif
                 @if (count($articles))
                     <li class="nav-item">
-                        <a class="nav-link active" href="#newest_posts" role="tab" data-bs-toggle="tab">New</a>
+                        <a class="nav-link @if (!count($featured_articles)) active @endif" href="#top_articles"
+                            role="tab" data-bs-toggle="tab">Top</a>
                     </li>
                 @endif
                 <li class="nav-item">
-                    <a class="nav-link" href="#authors_list" role="tab" data-bs-toggle="tab">Authors</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#categories" role="tab" data-bs-toggle="tab">Categories</a>
+                    <a class="nav-link @if (!count($featured_articles) && !count($articles)) active @endif" href="#authors_list"
+                        role="tab" data-bs-toggle="tab">Authors</a>
                 </li>
             </ul>
         </div>
 
         <div class="tab-content">
+            @if (count($featured_articles))
+                <div class="tab-pane fade @if (count($featured_articles)) show active @endif" id="featured">
+                    <ul class="list-unstyled sidebar-list d-table">
+                        @foreach ($featured_articles as $article)
+                            <li class="d-table-row">
+                                <div class="thumbnail d-table-cell">
+                                    <a href="{{ url('/show/' . $article->slug) }}">
+                                        <img src="{{ asset('images/articles/' . $article->image) }}"
+                                            alt="{{ $article->title }}" class="img-thumbnail">
+                                    </a>
+                                </div>
+                                <div class="text d-table-cell">
+                                    <h3><a href="{{ url('/show/' . $article->slug) }}">{{ $article->title }}</a></h3>
+                                    <p>{{ Str::limit($article->short_description, 90, ' [...]') }}</p>
+                                </div>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
             @if (count($articles))
-                <div class="tab-pane active" id="newest_posts">
+                <div class="tab-pane fade @if (!count($featured_articles)) show active @endif" id="top_articles">
                     <ul class="list-unstyled sidebar-list d-table">
                         @foreach ($articles as $article)
                             <li class="d-table-row">
@@ -39,7 +64,8 @@
                     </ul>
                 </div>
             @endif
-            <div class="tab-pane" id="authors_list">
+
+            <div class="tab-pane fade @if (!count($featured_articles) && !count($articles)) show active @endif" id="authors_list">
                 <ul class="list-unstyled sidebar-list d-table">
                     @foreach ($authors as $author)
                         <li class="d-table-row">
@@ -60,16 +86,6 @@
                         </li>
                     @endforeach
                 </ul>
-            </div>
-            <div class="tab-pane" id="categories">
-                <div class="list-group list-group-flush">
-                    @foreach ($categories as $category)
-                        <a href="{{ url('/category/' . $category->id) }}"
-                            class="list-group-item list-group-item-action">
-                            <span>{{ $category->name }}</span>
-                        </a>
-                    @endforeach
-                </div>
             </div>
         </div>
     </div>
